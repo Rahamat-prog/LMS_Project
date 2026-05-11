2c67730c3b842e77608d6f512ecd502c9f3ca9e3
Rahamat-prog <rahamatshaikh216@gmail.com>
2026-05-11 21:55:25 +0530

    refactor: move cloudinary configuration to separate config file

--- server/server.js
+++ server/server.js
@@ -3,18 +3,10 @@ const {config} = require('dotenv');
 config();
 const app = require('./app');
 const connectionToDB = require('./config/dbConnection');
-const cloudinary  = require('cloudinary');
+require("./config/cloudinary");
 
 
 const PORT =    process.env.PORT || 5000
-
-// cloudinary configurtion 
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
 
 app.listen(PORT, async () => {
     await connectionToDB();
